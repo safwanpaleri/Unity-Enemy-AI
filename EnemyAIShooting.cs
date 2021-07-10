3941039bf54e88f7d6ace99c10930c0c9a0e7a7d
safwanpaleri <safwanpaleri@gmail.com>
2021-07-10 21:05:24 +0530

    Update and rename EnemyAI.cs to EnemyAIShooting.cs

--- EnemyAIShooting.cs
+++ EnemyAIShooting.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAIShooting : MonoBehaviour
 {
     [Header("GameObjects")]
     [SerializeField] Player player;
